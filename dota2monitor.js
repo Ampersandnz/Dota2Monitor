435faeced566584540017f60ac43d1adc6a084f4
Michael Lo <nz.ampersand@gmail.com>
2016-11-21 09:45:14 +1300

    Set up dictionaries to map in-game hero and item names to their console command reference names. Working on ability names.

--- dota2monitor.js
+++ dota2monitor.js
@@ -2,12 +2,16 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var d2gsi = require('dota2-gsi');
+var fs = require("fs");
+var itemsJSON = require("./data/items");
+var heroesJSON = require("./data/heroes");
+var abilitiesJSON = require("./data/abilities");
 var server = new d2gsi();
 var clients = [];
 
-var heroes = []; // Dict to map console object name -> Actual hero name
-var items = []; // Dict to map console object name -> Actual item name
-var abilities = []; // Dict to map console object name -> Actual ability name
+var heroes = {}; // Dict to map console object name -> Actual hero name
+var items = {}; // Dict to map console object name -> Actual item name
+var abilities = {}; // Dict to map console object name -> Actual ability name
 
 server.events.on('newclient', function(client) {
     var update = "New client connection, IP address: " + client.ip + ", Auth token: " + client.auth.token;
@@ -23,14 +27,14 @@ server.events.on('newclient', function(client) {
     });
 
     client.on('hero:level', function(level) {
-        update = client.gamestate.hero.name + " is now level " + client.gamestate.hero.level;
+        update = heroes[client.gamestate.hero.name] + " is now level " + client.gamestate.hero.level;
         console.log(update);
         io.emit('d2gsi general update', update);
     });
 
     client.on('abilities:ability0:can_cast', function(can_cast) {
         if (can_cast) {
-            update = client.gamestate.abilities.ability0.name + " [" + client.gamestate.abilities.ability0.level + "] off cooldown!";
+            update = abilities[client.gamestate.abilities.ability0.name] + " [" + client.gamestate.abilities.ability0.level + "] off cooldown!";
             console.log(update);
             io.emit('d2gsi general update', update);
         }
@@ -38,7 +42,7 @@ server.events.on('newclient', function(client) {
 
     client.on('abilities:ability1:can_cast', function(can_cast) {
         if (can_cast) {
-            update = client.gamestate.abilities.ability1.name + " [" + client.gamestate.abilities.ability1.level + "] off cooldown!";
+            update = abilities[client.gamestate.abilities.ability1.name] + " [" + client.gamestate.abilities.ability1.level + "] off cooldown!";
             console.log(update);
             io.emit('d2gsi general update', update);
         }
@@ -46,7 +50,7 @@ server.events.on('newclient', function(client) {
 
     client.on('abilities:ability2:can_cast', function(can_cast) {
         if (can_cast) {
-            update = client.gamestate.abilities.ability2.name + " [" + client.gamestate.abilities.ability2.level + "] off cooldown!";
+            update = abilities[client.gamestate.abilities.ability2.name] + " [" + client.gamestate.abilities.ability2.level + "] off cooldown!";
             console.log(update);
             io.emit('d2gsi general update', update);
         }
@@ -54,7 +58,7 @@ server.events.on('newclient', function(client) {
 
     client.on('abilities:ability3:can_cast', function(can_cast) {
         if (can_cast) {
-            update = client.gamestate.abilities.ability3.name + " [" + client.gamestate.abilities.ability3.level + "] off cooldown!";
+            update = abilities[client.gamestate.abilities.ability3.name] + " [" + client.gamestate.abilities.ability3.level + "] off cooldown!";
 
             console.log(update);
             io.emit('d2gsi general update', update);
@@ -66,12 +70,12 @@ server.events.on('newclient', function(client) {
 
 setInterval(function() {
     clients.forEach(function(client, index) {
-        console.log(client.gamestate.hero.name + " GPM: " + client.gamestate.player.gpm + ", XPM: " + client.gamestate.player.xpm);
+        console.log(heroes[client.gamestate.hero.name] + " GPM: " + client.gamestate.player.gpm + ", XPM: " + client.gamestate.player.xpm);
 
         io.emit('d2gsi gpm update', client.gamestate.player.gpm);
         io.emit('d2gsi xpm update', client.gamestate.player.xpm);
     });
-}, 30 * 1000); // Every 30 seconds, update gpm and xpm
+}, 10 * 1000); // Every 10 seconds, update gpm and xpm
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
@@ -79,25 +83,22 @@ app.get('/', function(req, res) {
 
 http.listen(8081, function() {
     console.log("Ready to send events to clients on port 8081");
+
+	var itemsArray = itemsJSON.items[0];
+	var heroesArray = heroesJSON.heroes[0];
+	var abilitiesArray = abilitiesJSON.abilities[0];
+
+	for (var item in itemsArray) {
+	    items[item] = itemsArray[item];
+	}
+
+	for (var hero in heroesArray) {
+	    heroes[hero] = heroesArray[hero];
+	}
+
+	for (var ability in abilitiesArray) {
+	    abilities[ability] = abilitiesArray[ability];
+	}
+
+	console.log(abilities);
 });
-
-function setupHeroNames() {
-    heroes.push({
-        key:   "keyName",
-        value: "the value"
-    });
-}
-
-function setupItemNames() {
-    heroes.push({
-        key:   "keyName",
-        value: "the value"
-    });
-}
-
-function setupAbilityNames() {
-    heroes.push({
-        key:   "keyName",
-        value: "the value"
-    });
-}
