3cadc44da3860376b4ab2a2e8ee4f809197b752c
Michael Lo <nz.ampersand@gmail.com>
2016-11-23 11:38:49 +1300

    Added a bunch of events for items and abilities, and some placeholder logging to figure out possible values for some elements.

--- dota2monitor.js
+++ dota2monitor.js
@@ -16,7 +16,7 @@ var items = {}; // Dict to map console object name -> Actual item name
 var abilities = {}; // Dict to map console object name -> Actual ability name
 
 server.events.on('newclient', function(client) {
-    var update = "New client connection, IP address: " + client.ip;
+    var update = "New client connection, IP address: " + client.ip + (client.auth && client.auth.token ? ", Auth token: " + client.auth.token : "");
     console.log(update);
     io.emit('d2gsi message update', update);
 
@@ -53,6 +53,15 @@ server.events.on('newclient', function(client) {
     client.on('abilities:ability2:can_cast', function(can_cast) {
         if (can_cast) {
             update = abilities[client.gamestate.abilities.ability2.name] + " [" + client.gamestate.abilities.ability2.level + "] off cooldown!";
+            console.log(update);
+            io.emit('d2gsi message update', update);
+        }
+    });
+
+    client.on('abilities:ability3:can_cast', function(can_cast) {
+        if (can_cast) {
+            update = abilities[client.gamestate.abilities.ability3.name] + " [" + client.gamestate.abilities.ability3.level + "] off cooldown!";
+
             console.log(update);
             io.emit('d2gsi message update', update);
         }
@@ -112,15 +121,6 @@ server.events.on('newclient', function(client) {
         }
     });
 
-    client.on('abilities:ability3:can_cast', function(can_cast) {
-        if (can_cast) {
-            update = abilities[client.gamestate.abilities.ability3.name] + " [" + client.gamestate.abilities.ability3.level + "] off cooldown!";
-
-            console.log(update);
-            io.emit('d2gsi message update', update);
-        }
-    });
-
     client.on('player:gpm', function(gpm) {
         console.log("GPM: " + gpm);
         io.emit('d2gsi gpm update', gpm);
@@ -146,8 +146,125 @@ server.events.on('newclient', function(client) {
         io.emit('d2gsi a update', assists);
     });
 
-    //TODO: On hero.name, set the hero and ability icons
-    //TODO: On item slot change, set the item icon
+    client.on('hero:name', function(name) {
+        var hero = heroes[name];
+        console.log("Hero picked: " + hero);
+    	io.emit('d2gsi hero update', hero);
+    });
+
+    client.on('abilities:ability1:name', function(name) {
+        var ability = abilities[name];
+		io.emit('d2gsi ability1 update', ability);
+
+		var update = "Ability " + ability + " acquired. TODO: FIND OUT IF THIS IS EMITTED ON THE FIRST SKILL POINT OR ON PICK";
+        console.log(update);
+        io.emit('d2gsi message update', update);
+    });
+
+    client.on('abilities:ability2:name', function(name) {
+        var ability = abilities[name];
+		io.emit('d2gsi ability2 update', ability);
+
+		var update = "Ability " + ability + " acquired. TODO: FIND OUT IF THIS IS EMITTED ON THE FIRST SKILL POINT OR ON PICK";
+        console.log(update);
+        io.emit('d2gsi message update', update);
+    });
+
+    client.on('abilities:ability3:name', function(name) {
+        var ability = abilities[name];
+		io.emit('d2gsi ability3 update', ability);
+
+		var update = "Ability " + ability + " acquired. TODO: FIND OUT IF THIS IS EMITTED ON THE FIRST SKILL POINT OR ON PICK";
+        console.log(update);
+        io.emit('d2gsi message update', update);
+    });
+
+    client.on('abilities:ability4:name', function(name) {
+        var ability = abilities[name];
+		io.emit('d2gsi ability4 update', ability);
+
+		var update = "Ability " + ability + " acquired. TODO: FIND OUT IF THIS IS EMITTED ON THE FIRST SKILL POINT OR ON PICK";
+        console.log(update);
+        io.emit('d2gsi message update', update);
+    });
+
+    client.on('items:slot1:name', function(name) {
+        var item = items[name];
+		io.emit('d2gsi item1 update', item);
+
+		var update = "Item " + item + " acquired.";
+        console.log(update);
+        io.emit('d2gsi message update', update);
+    });
+
+    client.on('items:slot2:name', function(name) {
+        var item = items[name];
+		io.emit('d2gsi item2 update', item);
+
+		var update = "Item " + item + " acquired.";
+        console.log(update);
+        io.emit('d2gsi message update', update);
+    });
+
+    client.on('items:slot3:name', function(name) {
+        var item = items[name];
+		io.emit('d2gsi item3 update', item);
+
+		var update = "Item " + item + " acquired.";
+        console.log(update);
+        io.emit('d2gsi message update', update);
+    });
+
+    client.on('items:slot4:name', function(name) {
+        var item = items[name];
+		io.emit('d2gsi item4 update', item);
+
+		var update = "Item " + item + " acquired.";
+        console.log(update);
+        io.emit('d2gsi message update', update);
+    });
+
+    client.on('items:slot5:name', function(name) {
+        var item = items[name];
+		io.emit('d2gsi item5 update', item);
+
+		var update = "Item " + item + " acquired.";
+        console.log(update);
+        io.emit('d2gsi message update', update);
+    });
+
+    client.on('items:slot6:name', function(name) {
+        var item = items[name];
+		io.emit('d2gsi item6 update', item);
+
+		var update = "Item " + item + " acquired.";
+        console.log(update);
+        io.emit('d2gsi message update', update);
+    });
+
+    client.on('map:gamestate', function(gamestate) {
+		var update = "map:gamestate - " + gamestate;
+        console.log(update);
+		io.emit('d2gsi message update', update);
+    });
+
+    client.on('map:win_team', function(win_team) {
+		var update = "map:win_team - " + win_team;
+        console.log(update);
+		io.emit('d2gsi message update', update);
+    });
+
+    client.on('map:ward_purchase_cooldown', function(ward_purchase_cooldown) {
+		var update = "map:ward_purchase_cooldown - " + ward_purchase_cooldown;
+        console.log(update);
+		io.emit('d2gsi message update', update);
+    });
+
+    client.on('player:team_name', function(team_name) {
+		var update = "player:team_name - " + team_name;
+        console.log(update);
+		io.emit('d2gsi message update', update);
+    });
 
     clients.push(client);
 });
@@ -156,25 +273,24 @@ setInterval(function() {
     clients.forEach(function(client, index) {
     });
 
-//Just test data
     io.emit('d2gsi message update', "Test");
 
-    io.emit('d2gsi hero update', "Abaddon");
-    io.emit('d2gsi ability1 update', "Mist Coil");
-    io.emit('d2gsi ability2 update', "Aphotic Shield");
-    io.emit('d2gsi ability3 update', "Curse of Avernus");
-    io.emit('d2gsi ability4 update', "Borrowed Time");
-    io.emit('d2gsi item1 update', "Aghanim's Scepter");
-    io.emit('d2gsi item2 update', "Daedalus");
-    io.emit('d2gsi item3 update', "Tango");
-    io.emit('d2gsi item4 update', "Gem of True Sight");
-    io.emit('d2gsi item5 update', "Scythe of Vyse");
-    io.emit('d2gsi item6 update', "Sange and Yasha");
-    io.emit('d2gsi gpm update', "467");
-    io.emit('d2gsi xpm update', "591");
-    io.emit('d2gsi k update', "7");
-    io.emit('d2gsi d update', "3");
-    io.emit('d2gsi a update', "18");
+// Just test data
+    // io.emit('d2gsi ability1 update', "Mist Coil");
+    // io.emit('d2gsi ability2 update', "Aphotic Shield");
+    // io.emit('d2gsi ability3 update', "Curse of Avernus");
+    // io.emit('d2gsi ability4 update', "Borrowed Time");
+    // io.emit('d2gsi item1 update', "Aghanim's Scepter");
+    // io.emit('d2gsi item2 update', "Daedalus");
+    // io.emit('d2gsi item3 update', "Tango");
+    // io.emit('d2gsi item4 update', "Gem of True Sight");
+    // io.emit('d2gsi item5 update', "Scythe of Vyse");
+    // io.emit('d2gsi item6 update', "Sange and Yasha");
+    // io.emit('d2gsi gpm update', "467");
+    // io.emit('d2gsi xpm update', "591");
+    // io.emit('d2gsi k update', "7");
+    // io.emit('d2gsi d update', "3");
+    // io.emit('d2gsi a update', "18");
 //Will remove when the page layout/formatting is correct
 
 }, 1 * 1000); // Every second
